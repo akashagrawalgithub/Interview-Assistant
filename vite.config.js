971904faa767658c3b1aa035b6aa90e22c731a7e
akashagrawalgithub <akash242018@gmail.com>
2026-01-14 12:02:54 +0530

    Configure Vite for multiple pages and build

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                terms: resolve(__dirname, 'terms.html'),
+                privacy: resolve(__dirname, 'privacy.html'),
+                refund: resolve(__dirname, 'refund.html'),
+            },
+        },
+    },
+});
